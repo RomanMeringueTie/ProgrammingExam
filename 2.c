4007a664b6189567f8cd5eeb24ef676ea95db339
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-05-28 23:04:46 +0700

    2

---
+++ 2.c
@@ -0,0 +1,33 @@
+//  Определение префикса строки, состоящего только из допустимых символов.
+#include <stdio.h>
+
+int prefix(char *string, char *accept)
+{
+    int count = 0;
+    int isContinue = 1;
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        if (isContinue == 1)
+            for (int j = 0; accept[j] != '\0'; j++)
+            {
+                // printf("%c %d\n", string[i], count);
+                if (string[i] == accept[j])
+                {
+                    count++;
+                    isContinue = 1;
+                    break;
+                }
+                isContinue = 0;
+            }
+    }
+    return count;
+}
+
+int main()
+{
+    char *string = "romapasha";
+    char *accept = "artemochnev";
+    int result = prefix(string, accept);
+    printf("%d\n", result);
+    return 0;
+}
