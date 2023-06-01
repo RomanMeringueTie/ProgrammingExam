98938c05204fabdc373dc46a48c6f51d1f3bc86e
Pavel <Pasha04072004@yandex.ru>
2023-06-01 16:30:03 +0700

    God programming, n^2

--- 17.c
+++ 17.c
@@ -10,20 +10,22 @@ int str_len(char* number)
     return len;
 }
 
-int char_cnt(char* str, char* srcstr){
-	int cnt = 0;
-	for(int i = 0; i < str_len(str); i++){
-		for(int j = 0; j < str_len(srcstr); j++){
-			if(srcstr[j] == str[i]){
-				cnt++;
-			}
-		}
-	} 
-	return cnt;
+int char_cnt(char* str, char* srcstr)
+{
+    int cnt = 0;
+    for (int i = 0; i < str_len(str); i++) {
+        for (int j = 0; j < str_len(srcstr); j++) {
+            if (srcstr[j] == str[i]) {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
 }
 
-int main(){
-	char str[]="halo!";
-	char srcstr[] = "hello world!";
-	printf("%d\n", char_cnt(str,srcstr));
+int main()
+{
+    char str[] = "halo!";
+    char srcstr[] = "hello world!";
+    printf("%d\n", char_cnt(str, srcstr));
 }
