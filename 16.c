ffe4d587c17fab0cded2976e98e0f8417176d831
Pavel <Pasha04072004@yandex.ru>
2023-06-01 15:08:25 +0700

    Kaif

--- 16.c
+++ 16.c
@@ -11,16 +11,8 @@ int str_len(char* number)
     return len;
 }
 
-int main()
+void integer(int flag, int num, int sumchet, int sumnechet)
 {
-    char* number = malloc(100);
-    int num;
-    int flag = 0;
-    int sumchet = 0;
-    int sumnechet = 0;
-    int data;
-    scanf(" %s", number);
-    scanf(" %d", &num);
     while (flag == 0) {
         if (num == 0) {
             flag = 1;
@@ -33,8 +25,10 @@ int main()
         num /= 10;
     }
     printf("chet: %d, nechet: %d\n", sumchet, sumnechet);
-    sumchet = 0;
-    sumnechet = 0;
+}
+
+void chareger(char* number, int data, int sumchet, int sumnechet)
+{
     for (int i = 0; i < str_len(number); i++) {
         data = number[i] - 48;
         if (data % 2 == 0)
@@ -43,4 +37,20 @@ int main()
             sumnechet += data;
     }
     printf("chet: %d, nechet: %d\n", sumchet, sumnechet);
+}
+
+int main()
+{
+    char number[100];
+    int num;
+    int flag = 0;
+    int sumchet = 0;
+    int sumnechet = 0;
+    int data = 0;
+    scanf(" %s", number);
+    scanf(" %d", &num);
+    integer(flag, num, sumchet, sumnechet);
+    sumchet = 0;
+    sumnechet = 0;
+    chareger(number, data, sumchet, sumnechet);
 }
